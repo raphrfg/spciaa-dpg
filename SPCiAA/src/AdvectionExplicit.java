927bde878d1a630057d18a9ddb2b0c400c5e4827
Rafał Grzeszczuk <rafal.grzeszczuk@gmail.com>
2018-03-21 16:02:57 +0100

    finish project

--- SPCiAA/src/AdvectionExplicit.java
+++ SPCiAA/src/AdvectionExplicit.java
@@ -3,9 +3,8 @@ import java.util.concurrent.CyclicBarrier;
 public class AdvectionExplicit implements Problem {
     
     private static final double beta = 1;
-    
     private double F(double t) {
-        return Math.cos(2 * Math.PI * t);
+        return 2*Math.PI*Math.cos(2 * Math.PI * t);
     }
 
     @Override
@@ -13,6 +12,16 @@ public class AdvectionExplicit implements Problem {
         return new ABase(v, h, dt, t, barrier) {
             @Override
             Vertex apply(Vertex v) {
+
+                double a = beta* dt / (2 * h);
+                v.m_a[1][1] = 1.0;
+                v.m_a[1][2] = 0.0;
+                v.m_b[1] = 0 ;
+
+                v.m_a[2][1] = 0;
+                v.m_a[2][2] = 0.5;
+                v.m_b[2] = v.m_x[2]+a*v.m_x[1]+dt*F(t);
+
                 return v;
             }
         };
@@ -23,6 +32,14 @@ public class AdvectionExplicit implements Problem {
         return new ABase(v, h, dt, t, barrier) {
             @Override
             Vertex apply(Vertex v) {
+                double a = beta* dt / (2 * h);
+                v.m_a[1][1] = 0.5;
+                v.m_a[1][2] = 0.0;
+                v.m_b[1] = -a*v.m_x[2];
+
+                v.m_a[2][1] = 0;
+                v.m_a[2][2] = 0.5;
+                v.m_b[2] = v.m_x[2] + a * v.m_x[1] + dt * F(t);
                 return v;
             }
         };
@@ -33,6 +50,13 @@ public class AdvectionExplicit implements Problem {
         return new ABase(v, h, dt, t, barrier) {
             @Override
             Vertex apply(Vertex v) {
+                double a = beta* dt / (2 * h);
+                v.m_a[1][1] = 0.5;
+                v.m_a[1][2] = 0.0;
+                v.m_b[1] = -a*v.m_x[2];
+                v.m_a[2][1] = 0;
+                v.m_a[2][2] = 1;
+                v.m_b[2] = (1-2*a)*v.m_x[2] + 2*a * v.m_x[1] + dt * F(t);
                 return v;
             }
         };
--- SPCiAA/src/AdvectionImplicit.java
+++ SPCiAA/src/AdvectionImplicit.java
@@ -3,17 +3,25 @@ import java.util.concurrent.CyclicBarrier;
 public class AdvectionImplicit implements Problem {
 
     private static final double beta = 1;
-    
     private double F(double t) {
-        return Math.cos(2 * Math.PI * t);
+        return 2*Math.PI*Math.cos(2 * Math.PI * t);
     }
-
 
     @Override
     public Production makeA1(Vertex v, double h, double dt, double t, CyclicBarrier barrier) {
         return new ABase(v, h, dt, t, barrier) {
             @Override
             Vertex apply(Vertex v) {
+
+                double a = beta* dt / (2 * h);
+                v.m_a[1][1] = 1.0;
+                v.m_a[1][2] = 0.0;
+                v.m_b[1] = 0;
+
+                v.m_a[2][1] = 0;
+                v.m_a[2][2] = 0.5;
+                v.m_b[2] = v.m_x[2];
+
                 return v;
             }
         };
@@ -24,6 +32,14 @@ public class AdvectionImplicit implements Problem {
         return new ABase(v, h, dt, t, barrier) {
             @Override
             Vertex apply(Vertex v) {
+                double a = beta* dt / (2 * h);
+                v.m_a[1][1] = 0.5;
+                v.m_a[1][2] = a;
+                v.m_b[1] = dt*F(t+dt);
+
+                v.m_a[2][1] = -a;
+                v.m_a[2][2] = 0.5;
+                v.m_b[2] = v.m_x[2];
                 return v;
             }
         };
@@ -34,6 +50,13 @@ public class AdvectionImplicit implements Problem {
         return new ABase(v, h, dt, t, barrier) {
             @Override
             Vertex apply(Vertex v) {
+                double a = beta* dt / (2 * h);
+                v.m_a[1][1] = 0.5;
+                v.m_a[1][2] = a;
+                v.m_b[1] = dt*F(t+dt);
+                v.m_a[2][1] = -2*a;
+                v.m_a[2][2] = 1+2*a;
+                v.m_b[2] = v.m_x[2]+dt*F(t+dt);
                 return v;
             }
         };
